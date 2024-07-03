4ed625cf7b5f4a2fa474105c6d782fb6b1fd93a2
JorgeAvikla <jorge.avikla@gmail.com>
2024-07-03 17:27:35 -0600

    base url server

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  // baseUrl: 'http://10.19.121.76:8080'
-  baseUrl: 'https://credenciales.sistemasdif.tlaxcala.gob.mx'
+  baseUrl: 'http://10.19.121.76:8080'
+  // baseUrl: 'https://credenciales.sistemasdif.tlaxcala.gob.mx'
 }
+//
